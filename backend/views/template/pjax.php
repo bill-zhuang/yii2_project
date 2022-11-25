7c081dc148a485b6abc008eea69770c7bf77cddb
bill-zhuang <zstu_bill@outlook.com>
2022-11-25 10:56:42 +0800

    -remove search form pjax, fix widget js invalid after pjax

--- backend/views/template/pjax.php
+++ backend/views/template/pjax.php
@@ -10,9 +10,20 @@ use kartik\grid\GridView;
 $this->title = 'Pjax';
 $this->params['breadcrumbs'][] = $this->title;
 
+//pjax刷新后相应插件的js都会失效，需要重新注册 https://github.com/yiisoft/yii2-jui/issues/9
+//这里的对应的datepicker设置对应的参数-取pluginOptions
+$datepickerConfig = '{"format":"yyyy-mm-dd","todayHighlight":true,"autoclose":true,"language":"zh-CN"}';
+$js = "$(document).ready(function(){
+		$('#pjax_result').on('pjax:success', function(e) {
+		    jQuery('#datepicker_search_date').kvDatepicker($datepickerConfig); 
+		});
+    });";
+$this->registerJs($js, \yii\web\View::POS_END);
+
 ?>
 <?php \yii\widgets\Pjax::begin(['id' => 'pjax_result']) ?>
     <?= GridView::widget([
+        'id' => 'grid_result', //必须设置id，如果页面有多个gridview，同时用了pjax，会导致pjax刷新页面混乱
         'dataProvider' => $dataProvider,
         'responsive' => true,
         'responsiveWrap' => false,
--- backend/views/template/pjax_search.php
+++ backend/views/template/pjax_search.php
@@ -2,40 +2,46 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use kartik\date\DatePicker;
 
 /* @var $this yii\web\View */
 /* @var $form yii\widgets\ActiveForm */
 /* @var $params array */
 ?>
-<?php
-$this->registerJs(
-    '$("document").ready(function(){ 
-		$("#pjax_search_form").on("pjax:end", function() {
-			$.pjax.reload({container:"#pjax_result"});  //测试下来这个注释掉也不影响
-		});
-    });'
-);
-?>
 
-<?php \yii\widgets\Pjax::begin(['id' => 'pjax_search_form',]) ?>
-    <?php $form = ActiveForm::begin([
-        'id' => 'form-1',
-        'action' => ['pjax-search'],
-        'method' => 'get',
-        "options" => ['class' => 'form-inline', 'data-pjax' => true, 'autocomplete' => 'off'],
-        'fieldConfig' => [
-            'template' => "{input}",
-            'labelOptions' => [
-                'class' => 'control-label'
-            ]
+<?php $form = ActiveForm::begin([
+    'id' => 'form-1',
+    'action' => ['pjax-search'],
+    'method' => 'get',
+    //data-pjax为对应gridview外面包的pjax的id
+    "options" => ['class' => 'form-inline', 'data-pjax' => '#pjax_result', 'autocomplete' => 'off'],
+    'fieldConfig' => [
+        'template' => "{input}",
+        'labelOptions' => [
+            'class' => 'control-label'
         ]
+    ]
+]); ?>
+<div class="form-group">
+    <?php echo DatePicker::widget([
+        'id' => 'datepicker_search_date',
+        'name' => 'search_date',
+        'value' => $params['search_date'] ?? date('Y-m-d'),
+        'options' => ['placeholder' => '请选择查询日期'],
+        'pluginOptions' => [
+            'format' => 'yyyy-mm-dd',
+            'todayHighlight' => true,
+            'autoclose' => true,
+        ],
+        'removeButton' => false,
     ]); ?>
-    <div class="form-group">
-        <?= Html::submitButton('查询', ['class' => 'btn btn-primary']) ?>
-    </div>
-    <div class="form-group">
-        <?= Html::resetButton('重置', ['class'=>'btn btn-default form-group'])?>
-    </div>
+</div>
 
-    <?php ActiveForm::end(); ?>
-<?php \yii\widgets\Pjax::end(); ?>
+<div class="form-group">
+    <?= Html::submitButton('查询', ['class' => 'btn btn-primary']) ?>
+</div>
+<div class="form-group">
+    <?= Html::resetButton('重置', ['class'=>'btn btn-default form-group'])?>
+</div>
+
+<?php ActiveForm::end(); ?>
