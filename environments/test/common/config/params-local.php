2969620369f9810e3dff5bf4e63a97ae5169766a
bill-zhuang <zstu_bill@hotmail.com>
2024-06-21 16:56:49 +0800

    test params

--- environments/test/common/config/params-local.php
+++ environments/test/common/config/params-local.php
@@ -6,4 +6,18 @@ return [
         'bucket' => '',
         'end-point' => '',
     ],
+    'redis' => [
+        'host' => '127.0.0.1',
+        'port' => 6379,
+        'password' => null,
+    ],
+    'xsolla' => [
+        'merchant_id' => '',
+        'api_key' => '',
+        'project_id' => '',
+        'sign_key' => '',
+        'pay_token_url' => 'https://store.xsolla.com/api/v3/project/%s/admin/payment/token',
+        'sandbox_pay_url' => 'https://sandbox-secure.xsolla.com/paystation4/payment/3215?token=',
+        'prod_pay_url' => 'https://secure.xsolla.com/paystation4/payment/3215?token=',
+    ],
 ];
